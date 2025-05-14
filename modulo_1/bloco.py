e2c8303892ef2ae937c4f8e4ea8c38e16eb57d8c
Janyson Marques <janyson29@hotmail.com>
2025-05-14 10:54:31 -0300

    Commit inicial

---
+++ modulo_1/bloco.py
@@ -0,0 +1,6 @@
+idade = 25
+
+if idade < 18:
+  print("menos de idade")
+else:
+  print("maior de idade")
---
+++ modulo_1/sintaxe.py
@@ -0,0 +1,6 @@
+# Esse é o meu primeiro comentário
+"""
+É outro tipo de comentário
+"""
+
+nome = "Janyson" + "Silva"
