43df36cf7b1ff5d824bdde26a5831ee149cfd3fa
Janyson Marques <janyson29@hotmail.com>
2025-05-14 12:06:56 -0300

    numeros

---
+++ modulo_1/tipos_variaveis/numeros.py
@@ -0,0 +1,11 @@
+#Inteiro
+numero_inteiro = 3
+print("Inteiro =", + numero_inteiro)
+
+#Real com ponto flutuante
+numero_real = 3.14
+print('Real com flutuante =', + numero_real)
+
+#Type()
+print('Tipo da variavel inteira =', type(numero_inteiro))
+print('Tipo da variavel real com flutuante =', type(numero_real))
---
+++ modulo_1/variaveis.py
@@ -0,0 +1,6 @@
+#SnakeCase
+nome_completo = "Janyson Silva"
+idade = 25
+
+#CamelCase
+nomeCompleto = "Janyson Marques"
