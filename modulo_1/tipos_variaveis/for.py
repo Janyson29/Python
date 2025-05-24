05c940385c9b2db1be3091e85f8171eb8410b4da
Janyson Marques <janyson29@hotmail.com>
2025-05-24 00:05:18 -0300

    add for

--- modulo_1/tipos_variaveis/for.py
+++ modulo_1/tipos_variaveis/for.py
@@ -20,3 +20,26 @@ for valor in pessoa.values():
 print("\n For utilizando dicionário - itens")
 for chave, valor in pessoa.items():
   print(f"{chave}: {valor}")
+
+#Range(): retorna um intervalo númerico
+#[0,1,2,3,4,5,6,7,8,9]
+print('\n Utilizando a função range()')
+for numero in range(5):
+  print('Número:', numero)
+
+print('\n Utilizando a função range()')
+lista = [1, 2, 3, 4, 5]
+print(lista)
+for indice in range(0, len(lista)):
+  print("Indice:", indice)
+  print("Elemento:", lista[indice])
+  if indice == 3:
+    lista[indice] = 5
+  else:
+    lista[indice] = 0
+print(lista)
+
+# enumerate()
+lista_enumerate = ["a", "b", "c"]
+for indice, valor in enumerate(lista_enumerate):
+  print(f"{indice}: {valor}")
