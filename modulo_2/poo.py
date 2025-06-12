17d1f6289f322cdb5455aeb3e3d71c31c852d4cd
Janyson Marques <janyson29@hotmail.com>
2025-06-11 21:15:40 -0300

    Update poo.py

--- modulo_2/poo.py
+++ modulo_2/poo.py
@@ -6,5 +6,18 @@ class Pessoa:
     self.nome = nome
     self.idade = idade
 
+  def saudacao(self):
+    return f"Olá, meu nome é {self.nome} e eu tenho {self.idade} anos"
+
 # Objetos
 pessoa1 = Pessoa("Alice", 30)
+mensagem = pessoa1.saudacao()
+print(mensagem)
+
+pessoa2 = Pessoa(nome='Rodrigo', idade=32)
+mensagem = pessoa2.saudacao()
+print(mensagem)
+
+pessoa3 = Pessoa('Pedro', 26)
+mensagem = pessoa3.saudacao()
+print(mensagem)
