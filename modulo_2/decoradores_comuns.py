8c39071c7598ae1835c97fe27a1f64a52988efbc
Janyson Marques <janyson29@hotmail.com>
2025-06-15 17:12:36 -0300

    Update decoradores_comuns.py

--- modulo_2/decoradores_comuns.py
+++ modulo_2/decoradores_comuns.py
@@ -14,8 +14,37 @@ class MinhaClasse:
   @classmethod
   def metodo_classe(cls):
     return f"Método de classe chamado para valor={cls.valor}"
+  
+  @staticmethod
+  def metodo_estatico():
+    return "Método estático"
 
 obj = MinhaClasse(nome="Classe exemplo")
 print(obj.metodo_instancia())
 print(MinhaClasse.valor)
 print(MinhaClasse.metodo_classe())
+print(MinhaClasse.metodo_estatico())
+
+class Carro:
+  def __init__(self, marca, modelo, ano):
+    self.marca = marca
+    self.modelo = modelo 
+    self.ano = ano 
+
+  @classmethod
+  def criar_carro(cls, configuracao):
+    marca, modelo, ano = configuracao.split(",")
+    return cls(marca, modelo, int(ano))
+
+configuracao1 = "Toyota,Corrola,2025"
+carro1 = Carro.criar_carro(configuracao1)
+print(f"Marca: {carro1.marca}\nModelo: {carro1.modelo}\nAno: {carro1.ano}")
+
+class Matematica:
+
+  @staticmethod
+  def somar(a, b):
+    return a + b
+  
+print(Matematica.somar(a=10, b=20))
+  
