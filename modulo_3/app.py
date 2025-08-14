1feb530892fad8a4942ed51c87737fa24d432363
Janyson Marques <janyson29@hotmail.com>
2025-08-14 20:19:52 -0300

    Update app.py

--- modulo_3/app.py
+++ modulo_3/app.py
@@ -38,5 +38,24 @@ def get_task(id):
     
   return jsonify({"messege": "Não foi possível encontrar a atividade"}), 404
 
+@app.route('/tasks/<int:id>', methods=["PUT"])
+def update_task(id):
+  task = None
+  for t in tasks:
+    if t.id == id:
+        task = t
+  print(task)
+
+  if task ==None:
+    return jsonify({"messege": "Não foi possível encontrar a atividade"}), 404
+
+  data = request.get_json()
+  task.title = data['title']
+  task.description = data['description']
+  task.completed = data['completed']
+  print(task)
+  return jsonify({'messege': 'Tarefa atualizada com sucesso'})
+
+
 if __name__ == "__main__":
   app.run(debug=True)
